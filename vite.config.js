86f31a2f45d86c8d40c56ab5ef7e8128d8339919
tapcard25 <tu-tapcarf25@gmail.com>
2025-10-06 20:41:35 -0300

    CreaTuLanding+Colletti

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/reactjsColletti/", // ⬅️ AGREGAR ESTO (nombre de tu repo)
+  base: "/reactjsColletti/",
 });
